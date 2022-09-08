d9a5c09855c06def1192cf79a5026181827547ba
Aworh Rex <rex.c.xander@gmail.com>
2022-09-08 11:23:47 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *  *
+ *  * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	printf(sizeof(d)\n);
+	printf(sizeof(a)\n);
+	printf(sizeof(b)\n);
+	printf(sizeof(c)\n);
+	printf(sizeof(f)\n);
+	return (0);
+}
