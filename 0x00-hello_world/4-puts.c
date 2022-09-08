5230497ec277e0a59155b9e55e3cdf06bbe94fd7
Aworh Rex <rex.c.xander@gmail.com>
2022-09-08 08:34:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main (void) 
+int main(void)
 {
 	puts(
-			"Programming is like building a multilingual puzzle\n"
+			""Programming is like building a multilingual puzzle\n""
 			);
-	return(0);
+	return (0);
 }
