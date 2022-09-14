52cb5d09747ed892b3d98a903828068665460330
Aworh Rex <rex.c.xander@gmail.com>
2022-09-14 06:09:53 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,41 @@
 * @n: integer argument
 */
 
+
 void print_to_98(int n)
+
 {
-	while (n < 98)
-	{
-		printf("%d, ", n);
-		n++;
-	}
-	while (n > 98)
-	{
-		printf("%d, ", n);
-		n--;
-	}
-	if (n == 98)
-		printf("%d", n);
-		printf("\n");
+
+		while (n < 98)
+
+				{
+
+							printf("%d, ", n);
+
+									n++;
+
+										}
+
+
+
+			while (n > 98)
+
+					{
+
+								printf("%d, ", n);
+
+										n--;
+
+											}
+
+
+
+				if (n == 98)
+
+							printf("%d", n);
+
+
+
+					printf("\n");
+
 }
