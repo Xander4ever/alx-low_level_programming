a6cb166f595a9d782d0847eb76a9ff185468d1c4
Aworh Rex <rex.c.xander@gmail.com>
2022-09-19 02:26:02 -0700

    j

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,14 +6,31 @@
 * @n: Pointer argument passed to function.
 * Return: 0
 */
+#include <stdio.h>
+
+
+
+void reset_to_98(int *);
+
+
+
+/**
+ *
+ *  * main - check the code for Holberton School students.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
 int main(void)
 {
-    int n;
+	int n;
 
-    	n = 402;
-    	printf("n=%d\n", n);
-    	reset_to_98(&n);
-	n=98;
-	printf("n=%d\n", n);
-        return (0);
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }
