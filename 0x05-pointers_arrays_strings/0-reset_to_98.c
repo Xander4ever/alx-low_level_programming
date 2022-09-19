0749ac7bdb05f8ebec9b1b9a4597486028148cab
Aworh Rex <rex.c.xander@gmail.com>
2022-09-19 02:43:24 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,45 +6,7 @@
 * @n: Pointer argument passed to function.
 * Return: 0
 */
-#include <stdio.h>
-
-
-
-void reset_to_98(int *);
-
-
-
-/**
- *
- *  * main - check the code for Holberton School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-#include "main.h"
-
-
-
-/**
- *
- *  * reset_to_98 - Takes a pointer to an int as parameter, updates the value
- *
- *   * it points to
- *
- *    * @n: Pointer argument passed to function.
- *
- *     * Return: 0
- *
- *      */
-
-
-
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+	*n = 98;
 }
