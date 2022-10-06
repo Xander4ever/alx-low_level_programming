11fe404e45f7c57d5eab3b56a521c295d770ae70
Aworh Rex <rex.c.xander@gmail.com>
2022-10-06 11:59:40 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,44 @@
-#include "main.h"
-
-#include <stdlib.h>
-
-/**
- *
- *  * malloc_checked - allocates memory.
- *
- *   * @b: amount of bytes.
- *
- *    *
- *
- *     * Return: pointer to the allocated memory.
- *
- *      * if malloc fails, status value is equal to 98.
- *
- *       */
-
-
-
-void *malloc_checked(unsigned int b)
-
-{
-
-		void *p;
-
-
-
-			p = malloc(b);
-
-				if (p == NULL)
-
-							exit(98);
-
-
-
-					return (p);
-
-}
+#include <stdlib.h> 
+  
+ /** 
+  * string_nconcat - Concatenate two strings using n amount of s2 
+  * @s1: First string 
+  * @s2: String to add to end of s1 
+  * @n: Amount of s2 to add to s1 
+  * 
+  * Return: pointer to new area in memory, NULL if it fails 
+  */ 
+ char *string_nconcat(char *s1, char *s2, unsigned int n) 
+ { 
+         char *nstr, *empt; 
+         unsigned int i, len, j; 
+         unsigned int size; 
+  
+         len = 0; 
+         empt = ""; 
+         if (s1 == NULL) 
+                 s1 = empt; 
+         if (s2 == NULL) 
+                 s2 = empt; 
+         while (s1[len] != '\0') 
+                 len++; 
+         size = (len + n) * sizeof(*nstr); 
+         nstr = malloc(size + 1); 
+         if (nstr == NULL) 
+                 return (NULL); 
+         i = 0; 
+         while (i < size && s1[i] != '\0') 
+         { 
+                 nstr[i] = s1[i]; 
+                 i++; 
+         } 
+         j = 0; 
+         while (i < size && s2[j] != '\0') 
+         { 
+                 nstr[i] = s2[j]; 
+                 i++; 
+                 j++; 
+         } 
+         nstr[i] = '\0'; 
+         return (nstr); 
+ }
